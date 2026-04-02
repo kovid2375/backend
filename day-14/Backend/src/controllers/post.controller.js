063d6ee56516104b734938ae482a917361f3521d
kovid2375 <kovidchouhan2019@gmail.com>
2026-04-02 23:15:49 +0530

    day-14-insta-clone

--- day-14/Backend/src/controllers/post.controller.js
+++ day-14/Backend/src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 const postModel= require("../models/post.model")
 const {toFile}= require("@imagekit/nodejs")
 const ImageKit=require("@imagekit/nodejs")
+const jwt = require('jsonwebtoken')
 
 
 
@@ -11,13 +12,49 @@ const imagekit=new ImageKit({
 async function createPost(req,res) {
 
     console.log(req.body,req.file);
+
+    const  token=req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message:"Unauthorized"
+        })
+
+    }
+
+    let decoded=null
+
+   try {
+    decoded=jwt.verify(token, process.env.JWT_SECRET)
+    console.log(decoded);
+   } catch (error) {
+    return res.status(401).json({
+        message:"Unauthorized access"
+    })
+   }
+
+    
+    
+
+
     const file= await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer),'file'),
-        fileName:"test"
+        fileName:"test",
+        folder:"instagram-clone"
     })
     
-    res.send(file)
     
+
+    const post = await postModel.create({
+        caption:req.body.caption,
+        imgUrl:file.url,
+        user:decoded.id
+    })
+    
+    res.status(201).json({
+        message:"post created successfully",
+        post
+    })
     
 }
 
