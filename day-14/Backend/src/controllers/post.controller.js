0268f704a009ff11d240bb96ab8656cff0da0a88
kovid2375 <kovidchouhan2019@gmail.com>
2026-04-03 00:29:09 +0530

    day-14-insta-clone

--- day-14/Backend/src/controllers/post.controller.js
+++ day-14/Backend/src/controllers/post.controller.js
@@ -58,4 +58,78 @@ async function createPost(req,res) {
     
 }
 
-module.exports={createPost};
+async function getPost(req,res) {
+
+
+    const  token=req.cookies.token
+
+
+    let decoded;
+
+   try {
+    decoded=jwt.verify(token, process.env.JWT_SECRET)
+    console.log(decoded);
+   } catch (error) {
+    return res.status(401).json({
+        message:"Unauthorized access"
+    })
+   }
+
+   const userId=decoded.id
+
+   const post = await postModel.find({
+    user:userId
+   })
+
+   res.status(200).json({
+    message:"Posts fetched successfully",
+    post
+   })
+}
+
+async function getPostDetails(req,res) {
+
+    const token=req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message:"Unauthorized access"
+        })
+    }
+
+    let decoded;
+
+   try {
+    decoded=jwt.verify(token, process.env.JWT_SECRET)
+    console.log(decoded);
+   } catch (error) {
+    return res.status(401).json({
+        message:"Unauthorized access"
+    })
+   }
+
+   const userId=decoded.id
+
+   const postId=req.params.postId
+   const post=await postModel.findById(postId)
+   if(!post){
+    return res.status(404).json({
+        message:"Post not found"
+    })
+   }
+
+   const isValidUser= post.user.toString() === userId
+
+   if(!isValidUser){
+    return res.status(403).json({
+        message:"forbidden Content"
+    })
+   }
+
+   res.status(200).json({
+    message:"Post fetched successfully",
+    post
+   })
+
+    
+}
+module.exports={createPost, getPost, getPostDetails};
--- day-14/Backend/src/routes/post.js
+++ day-14/Backend/src/routes/post.js
@@ -14,4 +14,13 @@ const upload = multer({storage:multer.memoryStorage()});
 
 postRouter.post("/",upload.single('image'),postController.createPost)
 
+
+
+//Get /auth/post-> [protected route] get all posts of a user
+postRouter.get("/",postController.getPost)
+
+
+
+// Get /post/details/:postId-> return an detail about specific post with the id. aslo check wether the post belongs to the user taht the request come from or not. if not return unauthorized access
+postRouter.get("/details/:postId",postController.getPostDetails)
 module.exports=postRouter;
