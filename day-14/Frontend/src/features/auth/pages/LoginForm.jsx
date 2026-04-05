fe7b2a3778f081c4574c06c577a10a3864f68d0b
kovid2375 <kovidchouhan2019@gmail.com>
2026-04-06 00:59:56 +0530

    day-14-insta-clone-frontend

--- day-14/Frontend/src/features/auth/pages/LoginForm.jsx
+++ day-14/Frontend/src/features/auth/pages/LoginForm.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const LoginForm = () => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        axios.post("http://localhost:3000/auth/login",{
+            username,
+            password
+        },{
+            withCredentials: true
+        }).then((res) => {
+            console.log(res.data)
+        })
+    }
+
   return (
     <main className='min-h-screen w-full flex items-center justify-center '>
         <div className='h-fit min-w-100 flex flex-col gap-8 '>
             <h1 className='font-bold text-3xl'>Login</h1>
-            <form className='flex flex-col gap-4'>
-                <input className='border-2 outline-0 px-6 py-4 rounded-2xl' type="text" name='username' placeholder='Enter Username'/>
-                <input className='border-2 outline-0 px-6 py-4 rounded-2xl' type="password" name='password' placeholder='Enter Password'/>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+                <input 
+                    className='border-2 outline-0 px-6 py-4 rounded-2xl' 
+                    type="text" 
+                    name='username' 
+                    placeholder='Enter Username'
+                    
+                    onInput={(e) => setUsername(e.target.value)}
+                />
+                <input 
+                    className='border-2 outline-0 px-6 py-4 rounded-2xl' 
+                    type="password" 
+                    name='password' 
+                    placeholder='Enter Password'
+                    
+                    onInput={(e) => setPassword(e.target.value)}
+                />
                 <button className=' outline-0 px-6 py-4 rounded-2xl bg-red-500 text-white cursor-pointer active:scale-95' type='submit'>Login</button>
             </form>
+            <p>Don't have an account? <Link className='text-blue-400' to='/register'>Register</Link></p>
         </div>
     </main>
   )
--- day-14/Frontend/src/features/auth/pages/RegisterForm.jsx
+++ day-14/Frontend/src/features/auth/pages/RegisterForm.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { use } from 'react'
 
 const RegisterForm = () => {
+
+
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    async function handleSubmit(e){
+        e.preventDefault()
+
+        await axios.post("http://localhost:3000/auth/register", {
+            username,
+            email,
+            password
+        }, {
+            withCredentials: true
+        }).then((res) => {
+            console.log(res.data)
+        })
+    }
+
+
   return (
-    <div>
-      register form
-    </div>
+    <main className='min-h-screen w-full flex items-center justify-center '>
+        <div className='h-fit min-w-100 flex flex-col gap-8 '>
+            <h1 className='font-bold text-3xl'>Register Form</h1>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+                <input onInput={(e) => setUsername(e.target.value)} className='border-2 outline-0 px-6 py-4 rounded-2xl' type="text" name='username' placeholder='Enter Username'/>
+                <input onInput={(e) => setEmail(e.target.value)} className='border-2 outline-0 px-6 py-4 rounded-2xl' type="email" name='email' placeholder='Enter Email'/>
+                <input onInput={(e) => setPassword(e.target.value)} className='border-2 outline-0 px-6 py-4 rounded-2xl' type="password" name='password' placeholder='Enter Password'/>
+                <button className=' outline-0 px-6 py-4 rounded-2xl bg-red-500 text-white cursor-pointer active:scale-95' type='submit'>Register</button>
+            </form>
+            <p >Already have an account? <Link className='text-blue-400' to='/login'>Login</Link></p>
+        </div>
+    </main>
   )
 }
 
