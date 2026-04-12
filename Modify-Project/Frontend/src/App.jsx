168f714f147b8f3b7acdecbd32cbea09b09aa69e
kovid2375 <kovidchouhan2019@gmail.com>
2026-04-12 17:36:00 +0530

    modify-start

---
+++ Modify-Project/Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import FaceExpression from './features/Expression/Components/FaceExpression'
+
+function App() {
+  
+
+  return (
+    <FaceExpression/>
+  )
+}
+
+export default App
