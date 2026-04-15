6db9f49ec9f11db700e546533350b2db704086cc
kovid2375 <kovidchouhan2019@gmail.com>
2026-04-15 23:36:06 +0530

    modify-backend

--- Modify-Project/Backend/src/routes/auth.routes.js
+++ Modify-Project/Backend/src/routes/auth.routes.js
@@ -13,6 +13,7 @@ router.post('/register',authcontroller.register)
 router.post('/login',authcontroller.login)
 router.get('/get-me',identifyuser,authcontroller.getme)
 router.get('/logout',authcontroller.logout)
+router.get('/loggggout',authcontroller.logout)
 
 
 
